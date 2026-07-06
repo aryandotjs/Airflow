c4a1d7df9610c9ffd1557bc5227ee9daa5371f47
Aryan Lohar <aryanloharchawand@gmail.com>
2026-07-06 10:58:52 -0300

    feat(ui/api): added api and ui for menu in the workflow component

--- main-backend/src/router/zap.ts
+++ main-backend/src/router/zap.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { authmiddleware } from "../middleware";
 import { prisma } from "../db";
 import { ZapCreateSchema } from "../types";
+import { ZapStatus } from "../generated/prisma/enums";
 
 export const zapRouter = Router()
 
@@ -63,6 +64,75 @@ zapRouter.get("/", authmiddleware, async (req, res) => {
     return res.json({ zaps })
 })
 
+zapRouter.post("/togglestatus", authmiddleware, async (req, res) => {
+    // const userId = (req as any).userId;
+    const userid = 3
+    const { crrstatus, workflowid } = req.body
+    let status = crrstatus;
+    console.log(crrstatus, workflowid)
+    try {
+
+        if (crrstatus === ZapStatus.PAUSED ||
+            crrstatus === ZapStatus.DRAFT) {
+            console.log("in first if")
+            status = ZapStatus.ACTIVE
+        }
+        console.log(crrstatus === ZapStatus.DRAFT, crrstatus, ZapStatus.PAUSED)
+
+        if (crrstatus == ZapStatus.ACTIVE) {
+            console.log("in sec if")
+
+            status = ZapStatus.PAUSED
+        }
+        const zap = await prisma.zap.updateMany({
+            where: {
+                id: workflowid,
+                userId: userid
+            },
+            data: {
+                status: status
+            }
+        })
+        return res.json({
+            zap,
+            msg: `Workflow ${status.toString().toLowerCase()}`
+        })
+
+    } catch (error) {
+        return res.json({
+            msg: `Failed Workflow ${crrstatus.toString().toLowerCase()}`
+        })
+    }
+
+})
+
+zapRouter.post("/togglestatus", authmiddleware, async (req, res) => {
+    // const userId = (req as any).userId;
+    const userid = 3
+    const { newname, workflowid } = req.body
+    try {
+        const response = await prisma.zap.update({
+            where: {
+                userId: userid,
+                id: workflowid
+            },
+            data: {
+                name: newname
+            }
+        })
+
+        return res.json({
+            msg: `name changed to ${newname}`
+        })
+
+    } catch (error) {
+        return res.json({
+            msg: `Failed changing name`
+        })
+    }
+
+})
+
 zapRouter.get("/all", async (req, res) => {
     // const userId = (req as any).userId;
 
@@ -97,6 +167,8 @@ zapRouter.get("/all", async (req, res) => {
     })
     return res.json({ zaps })
 })
+
+
 
 zapRouter.get("/available-triggers", async (req, res) => {
 
